e177560a69144782bb1c43b27ac249d5ea69a60e
Madi A. <sarabi@ncsu.edu>
2026-05-03 15:13:46 -0400

    adding un script

---
+++ run_job.sh
@@ -0,0 +1,18 @@
+# LSBATCH: User input
+#!/bin/bash
+#BSUB -n 1
+#BSUB -gpu "num=1:mode=exclusive_process:mps=yes"
+#BSUB -n 4
+#BSUB -W 600
+#BSUB -R "rusage[mem=8]"
+#BSUB -o output_file.j%J
+#BSUB -e error_file.j%J
+#BSUB -J my_python_job
+
+
+source ~/.bashrc
+conda activate  /usr/local/usrapps/iselingzhang/sarabi/tensorimage
+
+# Run your script
+python3 code/motivation_pilot.py --data-path data/Simulated\ Data/Simulated\ Data --n-repeats 10 --max-files 350
+
