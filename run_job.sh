55aaec456a8bf3e5b03aaa031321301378b8dec2
Madi A. <sarabi@ncsu.edu>
2026-06-09 20:28:14 -0400

    update code for the right name of real data

--- run_job.sh
+++ run_job.sh
@@ -12,13 +12,14 @@ source ~/.bashrc
 conda activate  /usr/local/usrapps/iselingzhang/sarabi/tensorimage
 
 # Run experiments
-DATA="data/Simulated Data/Simulated Data"
+#DATA="data/Simulated Data/Simulated Data"
 
-echo "=== RFTL-S ==="
-python3 code/rftl_s.py --data-path "$DATA" --n-repeats 10 --n-workers 8
+#echo "=== RFTL-S ==="
+#python3 code/rftl_s.py --data-path "$DATA" --n-repeats 10 --n-workers 8
 
-echo "=== RFTL-U ==="
-python3 code/rftl_u.py --data-path "$DATA" --n-repeats 10 --n-workers 8
+#echo "=== RFTL-U ==="
+#python3 code/rftl_u.py --data-path "$DATA" --n-repeats 10 --n-workers 8
 
-echo "=== RFTL-21 ==="
-python3 code/rftl_21.py --data-path "$DATA" --n-repeats 10 --n-workers 8
+#echo "=== RFTL-21 ==="
+#python3 code/rftl_21.py --data-path "$DATA" --n-repeats 10 --n-workers 8
+python code/rftl_s_real.py --data-path /rs1/researchers/x/xfang8/chapter3/Robust-Federated-Tensor-Learning/data/ --n-repeats 50 --n-workers 10 --output-dir /rs1/researchers/x/xfang8/chapter3/Robust-Federated-Tensor-Learning/output/
