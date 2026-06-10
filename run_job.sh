2e7420d20c8afcca472dc426f1bacf873bea4800
Madi A. <sarabi@ncsu.edu>
2026-06-10 18:08:12 -0400

    first result from full prediction with rftl_s

--- run_job.sh
+++ run_job.sh
@@ -1,7 +1,7 @@
 # LSBATCH: User input
 #!/bin/bash
 #BSUB -n 8
-#BSUB -W 1200
+#BSUB -W 2400
 #BSUB -R "rusage[mem=8]"
 #BSUB -o output_file.j%J
 #BSUB -e error_file.j%J
@@ -22,4 +22,4 @@ conda activate  /usr/local/usrapps/iselingzhang/sarabi/tensorimage
 
 #echo "=== RFTL-21 ==="
 #python3 code/rftl_21.py --data-path "$DATA" --n-repeats 10 --n-workers 8
-python code/rftl_s_real.py --data-path /rs1/researchers/x/xfang8/chapter3/Robust-Federated-Tensor-Learning/data/ --n-repeats 50 --n-workers 10 --output-dir /rs1/researchers/x/xfang8/chapter3/Robust-Federated-Tensor-Learning/output/
+python code/rftl_s_real.py --data-path /rs1/researchers/x/xfang8/chapter3/Robust-Federated-Tensor-Learning/data/ --n-repeats 10 --n-workers 10 --output-dir /rs1/researchers/x/xfang8/chapter3/Robust-Federated-Tensor-Learning/output/
